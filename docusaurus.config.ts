89e81cc9d35f34639c4865a6bab510738331fe68
Dawid Loranc <dawidloranc@gmail.com>
2024-07-06 18:43:36 +0200

    Add Algolia search

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -109,6 +109,38 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
       additionalLanguages: ['java', 'bash', 'lua', 'ini', 'shell-session', 'cpp'],
     },
+    algolia: {
+      // The application ID provided by Algolia
+      appId: 'NP5ZTDUP1T',
+
+      // Public API key: it is safe to commit it
+      apiKey: '36a1dfa06fb0be6ee1870344021f3a2c',
+
+      indexName: 'dloranc.github.io',
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      externalUrlRegex: 'external\\.com|domain\\.com',
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      // replaceSearchResultPathname: {
+      //   from: '/docs/', // or as RegExp: /\/docs\//
+      //   to: '/',
+      // },
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: 'search',
+
+      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+      insights: false,
+
+      //... other Algolia params
+    },
   } satisfies Preset.ThemeConfig,
 };
 
