b204006bb85a7efa998e747294faffb17994d04f
Dawid Loranc <dawidloranc@gmail.com>
2024-07-06 17:12:38 +0200

    Fix edit link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -49,7 +49,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/dloranc/dloranc.github.io/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/dloranc/dloranc.github.io/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
