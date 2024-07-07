9eb13639971265c278adfc2e334c678b0167a08e
Dawid Loranc <dawidloranc@gmail.com>
2024-07-07 22:27:12 +0200

    Add comments

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -39,6 +39,7 @@ const config: Config = {
       {
         docs: false,
         blog: {
+          blogPostComponent: '@theme/BlogPostPage',
           blogSidebarCount: 0,
           routeBasePath: '/',
           tagsBasePath: '/tags',
