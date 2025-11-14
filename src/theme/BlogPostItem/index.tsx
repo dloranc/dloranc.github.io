a76eef3e67af8ccb79f68c66b1a0f38171fa8b35
Dawid Loranc <dawidloranc@gmail.com>
2025-11-14 17:47:57 +0100

    Remove excerpts from home page

---
+++ src/theme/BlogPostItem/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import clsx from "clsx";
+import { useBlogPost } from "@docusaurus/theme-common/internal";
+import BlogPostItemContainer from "@theme/BlogPostItem/Container";
+import BlogPostItemHeader from "@theme/BlogPostItem/Header";
+import BlogPostItemContent from "@theme/BlogPostItem/Content";
+import BlogPostItemFooter from "@theme/BlogPostItem/Footer";
+import type { Props } from "@theme/BlogPostItem";
+
+export default function BlogPostItem({
+  children,
+  className,
+}: Props): JSX.Element {
+  const { isBlogPostPage } = useBlogPost();
+  const containerClassName = !isBlogPostPage ? "margin-bottom--xl" : undefined;
+
+  return (
+    <BlogPostItemContainer className={clsx(containerClassName, className)}>
+      <BlogPostItemHeader />
+      {isBlogPostPage && <BlogPostItemContent>{children}</BlogPostItemContent>}
+      <BlogPostItemFooter />
+    </BlogPostItemContainer>
+  );
+}
